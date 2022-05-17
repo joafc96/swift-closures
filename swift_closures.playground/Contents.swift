11801394257b85e1b5d590680695777342adf0e2
qbuser <qbuser@QB-IPL-2281.local>
2022-05-17 11:04:34 +0530

    closures in swift

---
+++ swift_closures.playground/Contents.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+private func code(withTitle: String, anothaFunc: () -> Void) {
+print("""
+___________________________________
+Results for: '\(withTitle)'
+""")
+    anothaFunc()
+}
+
+code(withTitle: "Simplest closure ever.") {
+    let sayHelloClosure = {
+        print("Hello Closure")
+    }
+    sayHelloClosure()
+}
+
+code(withTitle: "Closures with parameters.", anothaFunc: {
+    let sayHelloTo: (String) -> Void = {(name) in print("Bonjour pour mon ami \(name)")}
+     sayHelloTo("Jomon")
+})
+
+code(withTitle: "Closures that return value.", anothaFunc: {
+    let sumClosure: (Int, Int) -> Int = {(a, b) in return a + b}
+    let sumClosureAns = sumClosure(5, 6)
+     print(sumClosureAns)
+})
+
+code(withTitle: "Closures as function parameters.") {
+    func sayCapitalizedHello(for name: String, completion: (String) -> Void) {
+        let capName = name.capitalized
+        completion(capName)
+    }
+    sayCapitalizedHello(for: "jomon joshy george") {
+        (result) in
+        print(result)
+    }
+}
+
+private func escapingClosure(title: String, completion: @escaping (String) -> Void) {
+    print("______________________________")
+    print(title)
+    print("res")
+    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
+        completion("res after 2 secs")
+    }
+}
+
+escapingClosure(title: "Escaping closures.") { (response) in
+    print(response)
+}
